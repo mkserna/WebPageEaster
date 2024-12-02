98e0bf9cd646c6fb743fd27cfca1c282c6114885
mkserna <mperezserna8@gmail.com>
2024-12-02 08:40:22 -0500

    fix: add file vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    base: '/WebPageEaster/', // Reemplaza <REPO-NAME> con el nombre de tu repositorio
+    build: {
+        outDir: 'dist', // Carpeta de salida para el build
+    },
+};
